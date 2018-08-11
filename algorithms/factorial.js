be04315f705e37b2481488b1197623e4fdcb8f61
Ahamed <ahamedali95@gmail.com>
2018-08-10 22:54:07 -0400

    factorial problem

---
+++ algorithms/factorial.js
@@ -0,0 +1,17 @@
+// Write a function `factorial(n)`, that returns the factorial of the number `n`.
+// For example, the factorial of 4 is `4 * 3 * 2 * 1 = 24`.
+//
+// Examples:
+//
+// factorial(1); // => 1
+// factorial(4); // => 24
+// factorial(5); // => 120
+// factorial(10); // => 3628800
+
+const factorial = (n) => {
+  if(n <= 0) return 1;
+
+  return [...Array(n + 1).keys()].slice(2).reduce((accumulator, ele) => {
+    return accumulator *= ele;
+  }, 1);
+}
