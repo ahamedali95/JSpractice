1a3063ed8f2dd3f3ba01fe67e78b0b7a8926ad0e
Ahamed <ahamedali95@gmail.com>
2018-08-16 10:05:46 -0400

    nums to words

--- algorithms/P6/problems/5_nums_to_words.js
+++ algorithms/P6/problems/5_nums_to_words.js
@@ -11,7 +11,22 @@ numsToWords('159598') => 'OneFiveNineFiveNineEight'
 *******************************************************************************/
 
 function numsToWords(numString) {
-  // your code here...
+  const numToString = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+  };
+  
+  return numString.split("").reduce((accumulator, ele) => {
+    return accumulator += numToString[ele];
+  }, "");
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
