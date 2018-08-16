467fac91b842f9787a7000d413e1cb8bf9f400f9
Ahamed <ahamedali95@gmail.com>
2018-08-15 22:42:20 -0400

    greatest common factor problem

--- algorithms/P4/problems/3_greatest_common_factor!.js
+++ algorithms/P4/problems/3_greatest_common_factor!.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
 Write a function greatestCommonFactor(num1, num2) that returns the largest number
-that is divides both `num1` and `num2`.
+that divides both `num1` and `num2`.
 
 Examples:
 
@@ -10,7 +10,16 @@ greatestCommonFactor(7, 11) => 1
 *******************************************************************************/
 
 function greatestCommonFactor(num1, num2) {
-  // your code here...
+  let i = 0;
+  let gcf = null;
+
+  while(i < num1 && i < num2) {
+    if(num1 % i === 0 && num2 % i === 0) {
+      gcf = i;
+    }
+  }
+
+  return gcf;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
