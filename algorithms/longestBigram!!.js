7c03fea62353371c7b2107e61cf592f39812bb9e
Ahamed <ahamedali95@gmail.com>
2018-08-17 15:49:26 -0400

    longest bigram problem

--- algorithms/longestBigram!!.js
+++ algorithms/longestBigram!!.js
@@ -9,3 +9,24 @@
 // longestBigram("One must have a mind of winter"); // => 'must have'
 // longestBigram("go home to eat"); // => 'go home'
 // longestBigram("his last assessment is fun"); // => 'last assessment'
+
+//
+
+function longestBigram(sentence) {
+  const words = sentence.split(" ");
+  let longestBigram = "";
+  let longestBigramLen = 0;
+
+  for(let i = 0; i < words.length - 1; i++) {
+    const word1 = words[i];
+    const word2 = words[i+1];
+    const currentBigramLen = (word1 + word2).length;
+
+    if(currentBigramLen > longestBigramLen) {
+      longestBigramLen = currentBigramLen;
+      longestBigram = `${word1} ${word2}`;
+    }
+  }
+
+  return longestBigram;
+}
