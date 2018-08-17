6158d7b561b70b330de61d3376cab0045246a906
Ahamed <ahamedali95@gmail.com>
2018-08-17 18:39:26 -0400

    prime factors problem:

--- algorithms/P7/problems/1_prime_factors.js
+++ algorithms/P7/problems/1_prime_factors.js
@@ -12,7 +12,25 @@ primeFactors(11) => [11]
 *******************************************************************************/
 
 function primeFactors(n) {
-  // your code here...
+  const primeFactors = [];
+  let i = 0;
+
+  while(i <= n) {
+    if(n % i === 0 && isPrime(i)) primeFactors.push(i)
+    i++;
+  }
+
+  return primeFactors;
+}
+
+function isPrime(num) {
+  if(num < 2) return false;
+
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) return false;
+  }
+
+  return true;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
