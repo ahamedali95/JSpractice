8a72c5483537c3c77e157c634e42a456e43750ce
Ahamed <ahamedali95@gmail.com>
2018-08-18 19:01:01 -0400

    unique anagram problem

--- algorithms/isUniqueAnagram!!.js
+++ algorithms/isUniqueAnagram!!.js
@@ -27,3 +27,34 @@ function isUniqueAnagram(word1, word2) {
 // WHAT IF THERE IS REPEATED CHARACTERS IN EACH INPUT STRING?
 // For example:
 // isUniqueAnagram('iceeman', 'cinemap')
+//Our program above return true for this.
+
+function isUniqueAnagram(word1, word2) {
+  const tally = {};
+
+  for(let i = 0; i < word1.length; i++) {
+    const char = word1[i];
+
+    if(char in tally) {
+      tally[char]++;
+    } else {
+      tally[char] = 1;
+    }
+  }
+
+  for(let i = 0; i < word2.length; i++) {
+    const char = word1[i];
+
+    if(char in tally) {
+      tally[char]--;
+    } else {
+      return false;
+    }
+  }
+
+  for(const key in tally) {
+    if(tally[key]) return false;
+  }
+
+  return true;
+}
