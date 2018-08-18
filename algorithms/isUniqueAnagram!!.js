7bcf5ba8b1306daf4b2655145bd31a6eac61392e
Ahamed <ahamedali95@gmail.com>
2018-08-18 14:21:32 -0400

    is unique anagram problem

---
+++ algorithms/isUniqueAnagram!!.js
@@ -0,0 +1,25 @@
+// Write a function isUniqueAnagram(word1, word2) that
+// takes in two strings. The function should return true
+// if the strings are anagrams of each other, false otherwise.
+// Anagrams are strings that contain the same characters,
+// but not necessarily in the same order. Assume there
+// will be no repeated characters in each input string.
+//
+// Examples:
+//
+// isUniqueAnagram('iceeman', 'cinemae'); // => true
+// isUniqueAnagram('abcd', 'adcb'); // => true
+// isUniqueAnagram('abcd', 'adxb'); // => false
+// isUniqueAnagram('abcd', 'abcdx'); // => false
+
+function isUniqueAnagram(word1, word2) {
+  if(word1.length !== word2.length) return false;
+
+  for(let i = 0; i < word1.length; i++) {
+    const currentChar = word1[i];
+
+    if(word2.indexOf(currentChar) === -1) return false;
+  }
+
+  return true;
+}
