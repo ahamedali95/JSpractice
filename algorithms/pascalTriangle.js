b167fa05621eac7e6d04a2a867f031d4df788250
Ahamed <ahamedali95@gmail.com>
2018-08-19 15:10:37 -0400

    suggested problems

---
+++ algorithms/pascalTriangle.js
@@ -0,0 +1,35 @@
+/*#### Pascal's Triangle ####
+# This is an example of Pascal's Triangle:
+#
+#  depth
+#    0   |      1
+#    1   |     1 1
+#    2   |    1 2 1
+#    3   |   1 3 3 1
+#    4   |  1 4 6 4 1
+#
+#
+# Pascal's Triangle is a mathematical construct that follows a simple
+# rule: each number in the triangle is the sum of the two numbers
+# directly above-and-to-the-left and above-and-to-the-right of it.
+#
+# For example, to find the next row after [1,2,1]:
+#
+#         1       2       1
+#
+#     1       3       3       1
+#     |       |       |       |
+#  (0 + 1) (1 + 2) (2 + 1) (1 + 0)
+#
+#
+# Write a function that will return Pascal's Triangle at the given
+# depth. The triangle should be represented as an array of rows.
+#
+# pascals_triangle(0) => [[1]]
+# pascals_triangle(2) => [[1], [1,1], [1,2,1]]
+# pascals_triangle(4) => [[1], [1,1], [1,2,1], [1,3,3,1], [1,4,6,4,1]]
+*/
+
+function pascalsTriangle(depth) {
+
+}
