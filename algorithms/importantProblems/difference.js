09c5772e7bee975f162aa0c9525da691a426086c
Ahamed <ahamedali95@gmail.com>
2018-08-20 12:17:04 -0400

    difference problem

--- algorithms/importantProblems/difference.js
+++ algorithms/importantProblems/difference.js
@@ -9,3 +9,9 @@
 //arr1 = [1,3,3]
 //arr2 = [1,3]
 //difference(arr1, arr2) => []
+
+function difference(arr1, arr2) {
+  return arr1.filter(ele => {
+    if(arr2.indexOf(ele) === -1) return ele;
+  });
+}
