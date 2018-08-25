bf559be11b5ca6e59bda28a7ce9d6936dffe6cc2
Ahamed <ahamedali95@gmail.com>
2018-08-25 18:34:54 -0400

    isPangram problem

--- algorithms/importantProblems/pangram.js
+++ algorithms/importantProblems/pangram.js
@@ -6,13 +6,24 @@
 // “The quick brown fox jumps over the dog” is not a Pangram
 //[Doesn’t contains all the characters from ‘a’ to ‘z’, as ‘l’, ‘z’, ‘y’ are missing]
 
-function isPangram(str) {
-  const alphabets = [..."abcdefghijklmnopqrstuvwxyz"];
-  const newStr = str.toLowerCase().split(" ").join("");
+// function isPangram(str) {
+//   const alphabets = [..."abcdefghijklmnopqrstuvwxyz"];
+//   const newStr = str.toLowerCase().split(" ").join("");
+//
+//   for(let i = 0; i < alphabets.length; i++) {
+//     if(newStr.indexOf(alphabets[i]) === -1) return false;
+//   }
+//
+//   return true;
+// }
 
-  for(let i = 0; i < alphabets.length; i++) {
-    if(newStr.indexOf(alphabets[i]) === -1) return false;
+function isPangram(str) {
+  const newStr = str.toLowerCase().split(" ").join("");
+  const tally = {};
+
+  for(let i = 0; i < newStr.length; i++) {
+    if(!(newStr[i] in tally)) tally[newStr[i]] = 1;
   }
 
-  return true;
+  return Object.keys(tally).length === 26;
 }
