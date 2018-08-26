652740d42a8b5fea34a199e2300da85b6ce5af4a
Ahamed <ahamedali95@gmail.com>
2018-08-26 12:46:14 -0400

    pangram problem

--- algorithms/importantProblems/pangram.js
+++ algorithms/importantProblems/pangram.js
@@ -39,3 +39,20 @@ function findMissingCharacters(arr) {
 
   return alphabets.filter(alphabet => arr.indexOf(alphabet) === -1).sort();
 }
+
+
+// Write a function winningHand(hand1, hand2) that takes in
+// two strings representing a hand of 4 cards. Return
+// the string of the hand with the higher total score. If there
+// is a tie, return 'DRAW'. Possible cards are J, Q, K.
+//
+// J = 1 point
+// Q = 2 points
+// K = 3 points
+//
+// Examples:
+//
+// winningHand('JQKJ', 'QQJJ'); // => 'JQKJ'
+// winningHand('KJKJ', 'QQQK'); // => 'QQQK'
+// winningHand('JKJJ', 'QQJJ'); // => 'DRAW'
+// winningHand('KJKJ', 'QQQQ'); // => 'DRAW'
