00ef00563b2f0f31843e23f2f597d43ded4de085
Ahamed <ahamedali95@gmail.com>
2018-09-03 18:03:54 -0400

    isogram

--- algorithms/importantProblems/isogram!!.js
+++ algorithms/importantProblems/isogram!!.js
@@ -14,31 +14,34 @@
 // isogramMatcher("unpredictably", "hydromagnetic"); //=> [1, 8]
 
 function isogramMatcher(word1, word2) {
-  return [sameIndexMatch(word1, word2), sameLetterMatch(word1, word2)];
-}
-
-function sameIndexMatch(word1, word2) {
-  return word1.split("").reduce((accumulator, char, index) => {
-    return word2[index] === char ? ++accumulator : accumulator;
-  }, 0);
-}
-
-function sameLetterMatch(word1, word2) {
-  let letters = [];
-  let count = 0;
+  let countSamePositionLetters = 0;
+  let countDifferentPositionLetters = 0;
 
   for(let i = 0; i < word1.length; i++) {
-    const char1 = word1[i];
+    const currentChar = word1[i];
 
-    for(let j = 0; j < word2.length; j++) {
-      const char2 = word2[j];
-
-      if(char1 === char2 && letters.indexOf(char1) === -1) {
-        letters.push(char1);
-        count++;
-      }
+    if(currentChar === word2[i]) {
+      countSamePositionLetters++;
+    } else if(word2.indexOf(currentChar) !== -1) {
+      countDifferentPositionLetters++;
     }
   }
 
-  return count;
+  return [countSamePositionLetters, countDifferentPositionLetters];
 }
+// 
+// function isogramMatcher(word1, word2) {
+//   return [matchLettersSamePosition(word1, word2), matchLettersDifferentPosition(word1, word2)];
+// }
+//
+// function matchLettersSamePosition(word1, word2) {
+//   return [...word1].reduce((accumulator, ele, index) => {
+//     return ele === word2[index] ? ++accumulator : accumulator;
+//   }, 0);
+// }
+//
+// function matchLettersDifferentPosition(word1, word2) {
+//   return [...word1].reduce((accumulator, ele, index) => {
+//     return word2.indexOf(ele) !== -1 && ele !== word2[index] ? ++accumulator : accumulator;
+//   }, 0);
+// }
