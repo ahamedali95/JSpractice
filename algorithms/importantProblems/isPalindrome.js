5875ef8a6c882462c0ee9f0ba3576b7b3edb64df
Ahamed <ahamedali95@gmail.com>
2018-09-03 20:06:48 -0400

    isPalindrome

--- algorithms/importantProblems/isPalindrome.js
+++ algorithms/importantProblems/isPalindrome.js
@@ -1,19 +1,30 @@
-//BOB
-//mmnmm
-//RACEECAR
+/*******************************************************************************
+Write a function isPalindrome(sentence) that returns true if the characters of the
+sentence string form a palindrome, false otherwise. A palindrome is a word or
+phrase that is that is the same forwards and backwards.
 
+Examples:
 
-// function isPalindrome(string) {
-//   for(let i = 0; i < Math.ceil(string.length / 2); i++) {
-//     if(string[i] !== string[string.length - 1 - i]) return false;
-//   }
-//
-//   return true;
-// }
+isPalindrome('rats live on no evil star'); // => true
+isPalindrome('stella won no wallets'); // => true
+isPalindrome('racecar'); // => true
+isPalindrome('hello world'); // => false
+*******************************************************************************/
 
-function isPalindrome(string) {
-  if(string.length === 0) return true;
-  if(string[0] !== string[string.length - 1]) return false;
+function isPalindrome(str) {
+  const newStr = str.split(" ").join(""); //remove all spaces
+  const midPoint = Math.floor(newStr.length / 2);
 
-  return isPalindrome(string.slice(1, string.length - 1));
+  for(let i = 0; i < midPoint; i++) {
+    if(newStr[i] !== newStr[newStr.length - 1 - i]) return false;
+  }
+
+  return true;
+}
+
+function isPalindrome(str) {
+  if(!str.length) return true;
+  if(str[0] !== str[str.length - 1]) return false;
+
+  return isPalindrome(str.slice(1, str.length - 1))
 }
