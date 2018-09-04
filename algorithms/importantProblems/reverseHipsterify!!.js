be6115c59da9260fd549b8e515f00de6d1c3345a
Ahamed <ahamedali95@gmail.com>
2018-09-03 22:28:28 -0400

    reverse hipsterfy

--- algorithms/importantProblems/reverseHipsterify!!.js
+++ algorithms/importantProblems/reverseHipsterify!!.js
@@ -3,35 +3,64 @@
 //
 // Examples:
 //
-// reverseHipsterfy("proper"); // => 'prper'
+// reverseHipsterfy("proper"); // => 'prper' => reporp
 // reverseHipsterfy("proper tonic panther"); // => 'prper tnic pnther'
 // reverseHipsterfy("bootcamp prep"); // => 'btcamp prep'
 // reverseHipsterfy("towel flicker banana"); // => 'twel flcker bnna'
 // reverseHipsterfy("runner anaconda"); // => 'rnner ncnda'
 // reverseHipsterfy("turtle cheeseburger fries"); // => 'trtle chsbrger fres'
 
-function reverseHipsterfy(sentence) {
-  return sentence.split(" ").map(word => {
-    return hipsterfy(word);
+
+
+function reverseHipsterfy(str) {
+  return str.split(" ").map(ele => {
+    return hipsterfy(ele);
   }).join(" ");
 }
 
 function hipsterfy(word) {
-  const vowels = [..."aeiou"];
-  let newWord = "";
+  let reversedWord = word.split("").reverse().join("");
   let isTheFirstTime = true;
+  let newStr = "";
+  const vowels = [..."aeiou"];
 
-  for(let i = word.length - 1; i >= 0; i--) {
-    const currentChar = word[i];
-    const isAvowel = vowels.indexOf(currentChar) !== -1;
+  for(let i = 0; i < reversedWord .length; i++) {
+    const currentChar = reversedWord[i];
 
-    if(isTheFirstTime && isAvowel) {
-      newWord += currentChar;
+    if(isTheFirstTime && vowels.indexOf(currentChar) !== -1) {
       isTheFirstTime = false;
-    } else if(!isAvowel) {
-      newWord += currentChar;
+      newStr += currentChar;
+    } else if(vowels.indexOf(currentChar) === -1){
+      newStr += currentChar;
     }
   }
 
-  return newWord.split("").reverse().join("");
+  return newStr.split("").reverse().join("");
 }
+
+//
+// function reverseHipsterfy(sentence) {
+//   return sentence.split(" ").map(word => {
+//     return hipsterfy(word);
+//   }).join(" ");
+// }
+//
+// function hipsterfy(word) {
+//   const vowels = [..."aeiou"];
+//   let newWord = "";
+//   let isTheFirstTime = true;
+//
+//   for(let i = word.length - 1; i >= 0; i--) {
+//     const currentChar = word[i];
+//     const isAvowel = vowels.indexOf(currentChar) !== -1;
+//
+//     if(isTheFirstTime && isAvowel) {
+//       newWord += currentChar;
+//       isTheFirstTime = false;
+//     } else if(!isAvowel) {
+//       newWord += currentChar;
+//     }
+//   }
+//
+//   return newWord.split("").reverse().join("");
+// }
