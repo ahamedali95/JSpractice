32b4ca8c0333f9081e4a34d0e144127dd261bfdd
Ahamed <ahamedali95@gmail.com>
2018-09-04 15:33:53 -0400

    repeated chars

--- algorithms/importantProblems/repeatedChars!!.js
+++ algorithms/importantProblems/repeatedChars!!.js
@@ -10,6 +10,25 @@
 // repeatedChars("mississippi") => ["s", "s", "p"]
 // repeatedChars("SSassSS") => ["S", "s", "S"]
 
+function repeatedChars(str) {
+  const chars = [];
+  let count = 0;
+
+  for(let i = 0; i < str.length - 1; i++) {
+    const currentChar = str[i];
+    const nextChar = str[i+1];
+
+    if(currentChar === nextChar) {
+      count++;
+    } else {
+      cout = 0;
+    }
+
+    if(count === 1) chars.push(currentChar);
+  }
+
+  return count;
+}
 
 function repeatedChars(str) {
   const chars = [];
